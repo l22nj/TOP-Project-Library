25d1253241c7ca8b2d8b30f0959413b830422df3
l22nj <211811295+l22nj@users.noreply.github.com>
2025-07-17 00:04:37 +0300

    made books appear more beautifully

--- script.js
+++ script.js
@@ -20,6 +20,19 @@ function createBook(form) {
     return new Book(...bookProperties);
 }
 
+function addBook(book) {
+    myLibrary.push(book);
+}
+
+function removeBook(id) {
+    for (let i = 0; i < myLibrary.length; i++) {
+        if (myLibrary[i].id === id) {
+            myLibrary.splice(i, 1);
+            return;
+        }
+    } console.log("Error: book to remove not present in library");
+}
+
 function resetDisplayedBooks() {
     let displayedBooks = document.querySelector(".main .books");
     while (displayedBooks.lastElementChild) {
@@ -27,46 +40,80 @@ function resetDisplayedBooks() {
     }
 }
 
-function updateLibrary(book="") {
+function updateLibrary() {
     resetDisplayedBooks();
+    let displayedBooks = document.querySelector(".main > .books");
     for (let book of myLibrary) {
-        // let item = document.createElement("div");
-        // item.setAttribute("class", "filler");
-        // document.querySelector(".main > .books")
-        //     .appendChild(item);
         let bookContainer =  document.createElement("div");
         bookContainer.setAttribute("class", "book");
         bookContainer.setAttribute("id", book.id);
 
-        let bookImage = document.createElement("img");
-        bookImage.setAttribute("class", "bookImage");
+        let bookImageContainer =   document.createElement("div");
+        bookImageContainer.setAttribute("class", "bookImageContainer");
+        bookContainer.appendChild(bookImageContainer);
 
-        let url = book.url ? book.url : "";
-        bookImage.setAttribute("src", url);
+        let bookImage = document.createElement("img");
+        bookImageContainer.appendChild(bookImage);
+
+        let bookButtons = document.createElement("div");
+        bookButtons.setAttribute("class", "bookButtonContainer");
+        bookContainer.appendChild(bookButtons);
+
+        let editButton = document.createElement("button");
+        // editButton.setAttribute("class", "editButton");
+        editButton.innerText = 'Edit';
+        bookButtons.appendChild(editButton);
+
+        editButton.addEventListener("click", () => {
+            alert("Edit! Work in progress...");
+            updateLibrary();
+        })
+
+        let removeButton = document.createElement("button");
+        // removeButton.setAttribute("class", "removeButton");
+        removeButton.innerText = 'X'
+        bookButtons.appendChild(removeButton);
+
+        removeButton.addEventListener("click", () => {
+            removeBook(book.id);
+            updateLibrary();
+        });
+
+        if (book.url) bookImage.setAttribute("src", book.url);
         bookImage.setAttribute("alt", book.title);
 
-        //
-        // for (let prop in book) {
-        //     if (!prop) continue;
-        //     if (prop.)
-        // }
+        let titleText = document.createElement("p");
+        titleText.innerText = book.title;
+        bookContainer.appendChild(titleText);
 
-        document.querySelector(".main > .books").appendChild(bookContainer);
+        let authorText = document.createElement("p");
+        authorText.innerText = book.author;
+        bookContainer.appendChild(authorText);
+
+        if (book.pages) {
+            let pagesText = document.createElement("p");
+            pagesText.innerText = book.pages + " pages";
+            bookContainer.appendChild(pagesText);
+        }
+
+        displayedBooks.appendChild(bookContainer);
     }
 }
 
-const dummyBook1 = new Book("Paradise Lost", "John Milton");
-const dummyBook2 = new Book("Samson Agonistes", "John Milton");
+const dummyBook1 = new Book("Paradise Lost", "John Milton", 0);
+const dummyBook2 = new Book("Samson Agonistes", "John Milton", 0);
 
 const dummyBooks = [dummyBook1, dummyBook2];
 
 myLibrary.push(...dummyBooks);
 
+updateLibrary();
+
 const formAddBook = document.querySelector("#form-add-book");
 
 formAddBook.addEventListener("submit", function(event) {
     event.preventDefault();
-    myLibrary.push(createBook(this));
+    addBook(createBook(this));
     updateLibrary();
     this.reset();
 })
